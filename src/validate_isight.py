35589f7f7e0afb57e879781dc4e76bbdab603b50
Oliver Wahlen <oliver.wahlen@infinit-group.de>
2018-11-16 20:50:56 +0100

    fixed of imshow if now face is detected

--- src/validate_isight.py
+++ src/validate_isight.py
@@ -11,17 +11,6 @@ import face_extractor as extractor
 import facenet
 
 
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser(description='Learn face from isight camera and recognize it later')
-    parser.add_argument('--model', type=str,
-                        help='Path to a model protobuf (.pb) file',
-                        default='model/20181112-063708/20181112-063708.pb')
-    parser.add_argument('--detect_multiple_faces', type=bool,
-                        help='Detect and align multiple faces per image.',
-                        default=True)
-    return parser.parse_args(argv)
-
-
 def main(args):
     PREVIEW_WINDOW = 'preview'
     QUIT_KEY = 27  # Escape
@@ -46,7 +35,7 @@ def main(args):
 
             height = int(frame.shape[0] * WIDTH / frame.shape[1])
             frame = cv2.resize(frame, (WIDTH, height))  # resize the frame
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # convert frame to RGB space
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # convert frame to RGB space
 
             # analysis
             boxes = face_detector.detect(image)
@@ -58,9 +47,9 @@ def main(args):
                 if isinstance(anchor_embedding, np.ndarray):
                     distance = facenet.distance([embeddings[i]], [anchor_embedding])
                     if distance <= THRESHOLD:
-                        color = (0,255,0)
+                        color = (0, 255, 0)
                     else:
-                        color = (0,0,255)
+                        color = (0, 0, 255)
                     frame = cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
                     frame = cv2.putText(frame,
                                         str("{:.6f}".format(distance[0])),
@@ -70,7 +59,7 @@ def main(args):
                 else:
                     frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-                cv2.imshow(PREVIEW_WINDOW, frame)
+            cv2.imshow(PREVIEW_WINDOW, frame)
 
             # key handling
             key = cv2.waitKey(1)
@@ -80,6 +69,17 @@ def main(args):
                 anchor_embedding = embeddings[0]
 
     cv2.destroyWindow(PREVIEW_WINDOW)
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser(description='Learn face from isight camera and recognize it later')
+    parser.add_argument('--model', type=str,
+                        help='Path to a model protobuf (.pb) file',
+                        default='model/20181112-063708/20181112-063708.pb')
+    parser.add_argument('--detect_multiple_faces', type=bool,
+                        help='Detect and align multiple faces per image.',
+                        default=True)
+    return parser.parse_args(argv)
 
 
 if __name__ == "__main__":
